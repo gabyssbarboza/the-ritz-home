13d4f2edb8d06cb00f71cca6e137aef04cfb6a9a
gabys barboza <gabrielly.barboza@outlook.com>
2020-11-30 19:09:27 -0300

    the ritz

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
         },
         {
             "name":  "trabalhe conosco",
-            "href": "https://gabyssbarboza.github.io/the-ritz-trabalhe-conosco/"
+            "href": "https://gabyssbarboza.github.io/trabalhe-conosco.html"
         },
         {
             "name":  "nossas atrações",
